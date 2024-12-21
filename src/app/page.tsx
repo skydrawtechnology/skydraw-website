feae7924f5723c2e515c104486bb6f1123e56eb5
skydrawtechnology <skydrawtechnology@gmail.com>
2024-12-21 11:29:41 +0000

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
-const Home = () => {
-    return (
-        <h1>Hello world</h1>
-    )
+import axios from "axios";
+
+async function getPagesData() {
+    const response = await axios.get('http://localhost:3000/api/pages-route');
+    return response.data;
 }
+
+const Home = async () => {
+    const data = await getPagesData();
+
+    return (
+        <>
+            <h1>Hello Big Bird</h1>
+            <pre>{JSON.stringify(data, null, 4)}</pre>
+        </>
+    );
+};
 
 export default Home;
